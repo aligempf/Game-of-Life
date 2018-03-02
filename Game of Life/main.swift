99723dc8fe56a6b999e3f38bb033e1e64a8aa705
Alistair Gempf <aligempf@gmail.com>
2018-03-02 18:39:28 +0000

    Initial Commit

---
+++ Game of Life/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Game of Life
+//
+//  Created by Rali on 02/03/2018.
+//  Copyright © 2018 Alistair Gempf. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
